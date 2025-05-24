c0ad06ef368292b955bc5539b4b260492652ee71
Juansan404 <juansanchezantunezo@gmail.com>
2025-05-24 21:51:57 +0200

    He creado las clases GameObject y SpriteGameObject Sin acabar

---
+++ src/bpc/framework/consola/GameObject.java
@@ -0,0 +1,9 @@
+package bpc.framework.consola;
+
+import bpc.daw.consola.Consola;
+
+public abstract class GameObject implements ElementoJuego {
+    protected Juego juego;
+    protected Consola consola;
+    protected Escena escena;
+}
